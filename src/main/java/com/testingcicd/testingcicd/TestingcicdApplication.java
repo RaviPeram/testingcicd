0d934299e83a81a11ffc2a43faf821a8d9fc2059
Ravi Peram <ravi.peram@bennettig.com>
2022-04-06 17:41:06 -0400

    Initial commit

---
+++ src/main/java/com/testingcicd/testingcicd/TestingcicdApplication.java
@@ -0,0 +1,13 @@
+package com.testingcicd.testingcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingcicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingcicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testingcicd/testingcicd/TestingcicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testingcicd.testingcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingcicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
